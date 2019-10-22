1cc29670afde36f71bed81848473b90b2fcd5f6c
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-10-21 20:40:59 -0500

    keny

--- ejemplo/src/main/java/com/examole/kenyController/KenyController.java
+++ ejemplo/src/main/java/com/examole/kenyController/KenyController.java
@@ -1,8 +1,5 @@
 package com.examole.kenyController;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
