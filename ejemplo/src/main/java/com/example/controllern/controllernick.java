34ea0848cefcb8c26cdf731b0468433ef86370e1
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-10-21 20:45:59 -0500

    nick

--- ejemplo/src/main/java/com/example/controllern/controllernick.java
+++ ejemplo/src/main/java/com/example/controllern/controllernick.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nick")
 public class controllernick {
 	@GetMapping("/")
-	public String getid() {
+	public String nick() {
 		return "hola";
 	}
 }
